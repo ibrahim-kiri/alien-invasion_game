2b9f61dec1a209747be46129c3debac1e2ecc1a4
ibrahim-kiri <ibrahimkiri@gmail.com>
2026-04-23 11:01:47 +0300

    Ships can move from left to right and drop

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -88,6 +89,12 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
 
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update positions."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
@@ -115,6 +122,21 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
         
     
     def _update_screen(self):
